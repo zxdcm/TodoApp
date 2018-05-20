f2ee861500b2a4cc5978267ca41c6d01ac0c5283
Egor Kirilin <z@xd.cm>
2018-05-20 20:49:03 +0300

    Models migrated to sqlalchemy

--- models.py
+++ models.py
@@ -1,100 +1,116 @@
-from peewee import Model, SqliteDatabase, CharField, ForeignKeyField, DateTimeField
 from utils import EnumField, STATUS, PRIORITY
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    DateTime,
+    Table,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import  create_engine
 
-
+Base = declarative_base()
 DATABASE = 'todoapp.db'
-db_connection = SqliteDatabase(DATABASE)
+#db_connection = SqliteDatabase(DATABASE)
 
 
 class Database():
 
     def __init__(self, connectionstring):
+        self.session = sessionmaker(bind=create_engine())
         pass
         #database = SqliteDatabase(DATABASE)
 
     @staticmethod
-    def create_tables():
-        with db_connection:
-            db_connection.create_tables([Folder, Task, Group,
-                                         Event, User, UserToGroups,
-                                         FolderTaskEvents])
-
-    @staticmethod
     def set_up_connection():
-        db_connection.connect(reuse_if_open=True)
+        pass
+        #db_connection.connect(reuse_if_open=True)
 
 
-class ModelBase(Model):
-    class Meta:
-        database = db_connection
+# link tables later
+user_group_association_table = Table('user_groups', Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('group_id', Integer, ForeignKey('group.id'))
+)
+
+task_folder_association_table = Table('task_folders', Base.metadata,
+    Column('task_id', Integer, ForeignKey('task.id')),
+    Column('folder_id', Integer, ForeignKey('folder.id'))
+)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password_hash = Column(String)
+    password_salt = Column(String)
+    email = Column(String, unique=True)
 
 
-class User(ModelBase):
-
-    username = CharField(unique=True)
-    password_hash = None
-    password_salt = None
-    email = CharField(unique=True)
-
-
-class Folder(ModelBase):
-
-    name = CharField(unique='True')
-    owner = ForeignKeyField(User, backref='folders')
+class Folder(Base):
+    __tablename__ = 'folders'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    owner = Column(Integer, ForeignKey('users.id'))
+    tasks = relationship(
+        'Task',
+        secondary=task_folder_association_table,
+        back_populates='folder')
 
 
-class Group(ModelBase):
-
-    name = CharField()
-    owner = ForeignKeyField(User, backref='groups')
-
-
-class Event(ModelBase):
-
-    name = CharField()
-    owner = ForeignKeyField(User, backref='events')
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    owner = Column(Integer, ForeignKey('users.id'))
 
 
-class Task(ModelBase):
-
-    name = CharField()
-    description = CharField()
-    owner = ForeignKeyField(User, backref='tasks')
-    parent_task = ForeignKeyField('self', null=True)
-    group = ForeignKeyField(Group, null=True)
-    status = EnumField(STATUS)
-    assigned = ForeignKeyField(User, null=True)
-    start_date = DateTimeField()
-    end_date = DateTimeField()
-    priority = EnumField(PRIORITY)
+# class Event(Base):
+#     __tablename__ = 'events'
+#     id = Column(Integer, primary_key=True)
+#     name = Column()
+#     owner = Column(Integer, ForeignKey('user.id'))
 
 
-class Cyclicity(ModelBase):
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    owner = Column(Integer, ForeignKey('users.id'))
+    parent_task = Column(Integer, ForeignKey('tasks.id'))
+    group = Column(Integer, ForeignKey('groups.id'))
+    #status = EnumField(STATUS)
+    assigned = Column(Integer, ForeignKey('users.id'))
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
 
-    period = DateTimeField()
-    duration = DateTimeField()
-    event = ForeignKeyField(Event, null=True)
-    task = ForeignKeyField(Task, null=True)
-
-
-class Notification(ModelBase):
-
-    date = DateTimeField()
-    event = ForeignKeyField(Event, null=True)
-    task = ForeignKeyField(Task, null=True)
-
-
-class UserToGroups(ModelBase):
-    user = ForeignKeyField(User, backref='groups')
-    group = ForeignKeyField(Group, backref='members')
-
-
-class FolderTaskEvents(ModelBase):
-    folder = ForeignKeyField(Folder)
-    task = ForeignKeyField(Task, null=True)
-    event = ForeignKeyField(Event, null=True)
+    folders = relationship(
+        'Folder',
+        secondary=task_folder_association_table,
+        back_populates='tasks'
+    )
 
 
-class Comment(ModelBase):
-    pass
+class Cyclicity(Base):
+    __tablename__ = 'cyclicities'
+    id = Column(Integer, primary_key=True)
+    period = Column(DateTime)
+    duration = Column(DateTime)
+    task = Column(Integer, ForeignKey('tasks.id'))
+    #event = ForeignKeyField(Event, null=True)
+
+
+# class Notification(Base):
+#     __tablename__ = 'notifications'
+#     id = Column(Integer, primary_key=True)
+#     date = Column(DateTime)
+#     task = Column(Integer, ForeignKey('task.id'))
+
+
+#
+# class Comment(ModelBase):
+#     pass
 
