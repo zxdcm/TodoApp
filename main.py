b41f38a21ea4eac4dc0d195d942e2ffa748f8bff
Egor Kirilin <z@xd.cm>
2018-08-02 00:27:39 +0300

    Parser to update task and check lib methods added.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from lib.services import AppService, Task, TaskPriority
 from datetime import datetime
 
@@ -15,29 +16,45 @@ class UserSession:
 def commands_handler(service: AppService, namespace, *args, **kwargs):
     if namespace.entity == 'task':
         if namespace.action == 'create':
-            service.create_task(user_id=namespace.user_id,
-                                name=namespace.name,
-                                description=namespace.description,
-                                start_date=namespace.start_date,
-                                end_date=namespace.end_date,
-                                parent_task_id=namespace.parent,
-                                priority=namespace.priority
-                                )
+            task = service.create_task(user_id=namespace.user_id,
+                                       name=namespace.name,
+                                       description=namespace.description,
+                                       start_date=namespace.start_date,
+                                       end_date=namespace.end_date,
+                                       parent_task_id=namespace.parent,
+                                       priority=namespace.priority)
+            print(task)
         elif namespace.action == 'show':
             if namespace.task_id == -1:
                 [print(x) for x in service.get_own_tasks(namespace.user_id)]
             else:
                 print(service.get_task(namespace.user_id,
                                        namespace.task_id))
+        elif namespace.action == 'share':
+            if namespace.user_receiver_id is None:
+                namespace.user_receiver_id = service.get_user_by_email(
+                    namespace.user_receiver_email).id
+            service.share_task(user_owner_id=namespace.user_id,
+                               task_id=namespace.task_id,
+                               user_receiver_id=namespace.user_receiver_id)
+        elif namespace.action == 'edit':
+            # rework
+            task_args = {'name': namespace.name,
+                         'description': namespace.description,
+                         'start_date': namespace.start_date,
+                         'end_date': namespace.end_date}
 
-    # if kwargs['action'] == 'create':
-    #     service.create_task(**kwargs)
-    # elif kwargs['action'] == 'delete':
-    #     service.delete_task_by_id(kwargs['id'])
-    # elif kwargs['action'] == 'list':
-    #     [print(x) for x in service.get_user_tasks()]
-    # elif kwargs['action'] == 'archive':
-    #     print(service.archive_task_by_id(kwargs['id']))
+            service.update_task(user_id=namespace.user_id,
+                                task_id=namespace.task_id,
+                                args=task_args)
+            # if kwargs['action'] == 'create':
+            #     service.create_task(**kwargs)
+            # elif kwargs['action'] == 'delete':
+            #     service.delete_task_by_id(kwargs['id'])
+            # elif kwargs['action'] == 'list':
+            #     [print(x) for x in service.get_user_tasks()]
+            # elif kwargs['action'] == 'archive':
+            #     print(service.archive_task_by_id(kwargs['id']))
 
 
 def func_task_parser(sup_parser: argparse):
@@ -63,9 +80,33 @@ def func_task_parser(sup_parser: argparse):
     # update = task_subparser.add_parser('update', help='Update task info',
     #                                    parents=[create])
 
-    show = task_subparser.add_parser('show', help='Show task info')
+    show = task_subparser.add_parser('show', help='Show task(s) info')
     show.add_argument('--task_id', type=int,
                       help='Task id', default=-1)
+
+    share_parent = task_subparser.add_parser('share',
+                                             help='Share task with user')
+    share_parent.add_argument('--task_id', required=True)
+    share = share_parent.add_mutually_exclusive_group(required=True)
+    share.add_argument('--user_receiver_id', type=int)
+    share.add_argument('--user_receiver_email',
+                       help='User email to share task with')
+
+    edit = task_subparser.add_parser('edit', help='Edit task with provided id')
+    edit.add_argument('--task_id', required=True)
+    edit.add_argument('--name')
+    edit.add_argument('--description')
+    edit.add_argument('--start_date')
+    edit.add_argument('--end_date')
+    edit.add_argument('--priority',
+                      choices=[x.name.lower() for x in TaskPriority])
+
+    # share = task_subparser.add_parser('share', help='Share task with user')
+    # share.add_argument('--task_id', required=True)
+    # share.add_argument('--user_receiver_id', type=int)
+    # share.add_argument('--user_email',
+    #                    help='User email to share task with')
+
     # delete = task_subparser.add_parser('delete', help='Delete task')
     # delete.add_argument('--id', type=int,
     #                     help='Task id')
@@ -84,9 +125,10 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    # AppService.create_user(name='Test', email='Test')
+    # AppService.create_user(name='Test2', email='newuser')
     main_parser = parse_args()
     args = main_parser.parse_args()
     service = AppService()
     args.user_id = 1
     commands_handler(service, args)
+    print(args)
