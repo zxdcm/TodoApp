2b243f5555ceb265d2563bd9ba09523b9645821a
Egor Kirilin <z@xd.cm>
2018-08-22 12:45:29 +0300

    Some args types specified. Search and filter args parsers added. Event field parser added.

--- client/parsers.py
+++ client/parsers.py
@@ -20,17 +20,39 @@ def valid_date(text):
             f'Invalid date format. Date example: "{datetime.now():%Y-%m-%d %H:%M}" ')
 
 
+def valid_future_date(text):
+    date = valid_date(text)
+    if date > datetime.now():
+        return date
+    else:
+        raise argparse.ArgumentTypeError(
+            'Invalid date. Date should be in future'
+        )
+
+
+def valid_int(x):
+    try:
+        x = int(x)
+        if x > 0:
+            return x
+        else:
+            raise ValueError()
+    except ValueError:
+        raise argparse.ArgumentTypeError('Not a positive number')
+
+
 def task_show_parser(show_subparser: argparse):
     task_show = show_subparser.add_subparsers(dest='show_type',
                                               title='Show tasks info',
                                               metavar='',
                                               description='Commands to show tasks')
+    task_show.required = True
 
     task_id = task_show.add_parser('id',
                                    help='Show task by id')
     task_id.add_argument('task_id',
                          help='Task id',
-                         type=int)
+                         type=valid_int)
 
     task_show.add_parser('own',
                          help='Show tasks created by user')
@@ -38,7 +60,7 @@ def task_show_parser(show_subparser: argparse):
     subtasks = task_show.add_parser('subtasks',
                                     help='Show subtasks by task id')
     subtasks.add_argument('task_id',
-                          type=int)
+                          type=valid_int)
 
     task_show.add_parser('all',
                          help='Show all tasks user can access')
@@ -46,14 +68,17 @@ def task_show_parser(show_subparser: argparse):
     task_show.add_parser('assigned',
                          help='Show tasks assigned on user')
 
-    task_show.add_parser('archived',
-                         help='Show archived tasks')
-
     task_show.add_parser('todo',
                          help='Show todo tasks')
 
+    task_show.add_parser('inwork',
+                         help='Show inwork tasks')
+
     task_show.add_parser('done',
                          help='Show done tasks')
+
+    task_show.add_parser('archived',
+                         help='Show archived tasks')
 
     task_show.add_parser('planless',
                          help='Show tasks without plan')
@@ -65,6 +90,7 @@ def task_parser(sup_parser: argparse):
     task_subparser = task_parser.add_subparsers(dest='action',
                                                 metavar='',
                                                 description='Commands to work with tasks')
+    task_subparser.required = True
 
     create = task_subparser.add_parser('create',
                                        help='Create new task')
@@ -75,15 +101,14 @@ def task_parser(sup_parser: argparse):
                         help='Task description')
     create.add_argument('-s', '--start_date',
                         type=valid_date,
-                        help='Start date (current time by default)',
-                        default=datetime.now())
+                        help='Start date (current time by default)')
     create.add_argument('-e',
                         '--end_date',
                         type=valid_date,
                         help='End date')
     create.add_argument('-p',
                         '--parent_task_id',
-                        type=int,
+                        type=valid_int,
                         help='Parent task id')
     create.add_argument('--priority',
                         type=str,
@@ -91,6 +116,8 @@ def task_parser(sup_parser: argparse):
     create.add_argument('--status',
                         type=str,
                         choices=[x.name.lower() for x in TaskStatus])
+    create.add_argument('--event', help='Mark as event',
+                        choices=['yes', 'no'])
 
     show = task_subparser.add_parser('show',
                                      help='Show tasks')
@@ -100,7 +127,7 @@ def task_parser(sup_parser: argparse):
                                      help='Edit task with provided id')
     edit.add_argument('-tid',
                       '--task_id',
-                      required=True, type=int)
+                      required=True, type=valid_int)
     edit.add_argument('-n',
                       '--name',
                       help='Task name')
@@ -119,12 +146,14 @@ def task_parser(sup_parser: argparse):
     edit.add_argument('--status',
                       type=str,
                       choices=['todo', 'inwork'])
+    edit.add_argument('--event', choices=['yes', 'no'],
+                      help='Mark as event')
 
     share = task_subparser.add_parser('share',
                                       help='Share task with user')
     share.add_argument('-tid',
                        '--task_id',
-                       type=int,
+                       type=valid_int,
                        help='Id of task to be shared',
                        required=True)
     share.add_argument('-ur',
@@ -139,7 +168,7 @@ def task_parser(sup_parser: argparse):
                          '--task_id',
                          help='Task id',
                          required=True,
-                         type=int)
+                         type=valid_int)
     unshare.add_argument('-urn',
                          '--user_receiver',
                          help='User name to unshare task with',
@@ -152,7 +181,7 @@ def task_parser(sup_parser: argparse):
                         '--task_id',
                         help='Task id',
                         required=True,
-                        type=int)
+                        type=valid_int)
     assign.add_argument('-urn',
                         '--user_receiver',
                         help='User name to assign task',
@@ -165,12 +194,12 @@ def task_parser(sup_parser: argparse):
                          '--task_id',
                          required=True,
                          help='Task id',
-                         type=int)
+                         type=valid_int)
     subtask.add_argument('-pid',
                          '--parent_task_id',
                          help='Parent task id',
                          required=True,
-                         type=int)
+                         type=valid_int)
 
     subtask = task_subparser.add_parser('rm_subtask',
                                         help='Remove relation between task and parent task')
@@ -178,7 +207,7 @@ def task_parser(sup_parser: argparse):
                          '--task_id',
                          required=True,
                          help='Task id',
-                         type=int)
+                         type=valid_int)
 
     archive = task_subparser.add_parser('archive',
                                         help='Archive task')
@@ -188,17 +217,42 @@ def task_parser(sup_parser: argparse):
 
     archive.add_argument('task_id',
                          help='task id',
-                         type=int)
+                         type=valid_int)
     done = task_subparser.add_parser('done',
                                      help='Done task')
     done.add_argument('task_id',
                       help='task id',
-                      type=int)
+                      type=valid_int)
+
+    search = task_subparser.add_parser('search',
+                                       help='Case insensitive task search by name')
+    search.add_argument('name', help='Task name')
+
+    filter = task_subparser.add_parser('filter',
+                                       help='Search tasks by specified params')
+    filter.add_argument('-n', '--name',
+                        help='Task name')
+    filter.add_argument('-d', '--description',
+                        help='Task description')
+    filter.add_argument('-s', '--start_date',
+                        type=valid_date,
+                        help='Task start date from')
+    filter.add_argument('-e', '--end_date',
+                        type=valid_date,
+                        help='Task end date till to')
+    filter.add_argument('-p', '--parent_task_id', type=valid_int,
+                        help='Parent task id')
+    filter.add_argument('--priority',
+                        choices=[x.name.lower() for x in TaskPriority],
+                        help='Task priority')
+    filter.add_argument('--status',
+                        choices=[x.name.lower() for x in TaskStatus],
+                        help='Task status')
 
     delete = task_subparser.add_parser('delete',
                                        help='Delete task with provided id')
     delete.add_argument('task_id',
-                        type=int)
+                        type=valid_int)
 
 
 def folder_show_parser(show_subparser: argparse):
@@ -206,10 +260,12 @@ def folder_show_parser(show_subparser: argparse):
                                               title='Show folders and its info',
                                               metavar='',
                                               description='Commands to show folders')
+    task_show.required = True
+
     folder_id = task_show.add_parser('id',
                                      help='Show folder by id')
     folder_id.add_argument('folder_id',
-                           help='Folder id', type=int)
+                           help='Folder id', type=valid_int)
     folder_id.add_argument('--tasks',
                            action='store_true',
                            help='Show folder tasks')
@@ -228,6 +284,7 @@ def folder_parser(sup_parser: argparse):
     folder_subparser = folder_parser.add_subparsers(dest='action',
                                                     metavar='',
                                                     description='Commands to work with folders')
+    folder_subparser.required = True
 
     create = folder_subparser.add_parser('create',
                                          help='Create new folder')
@@ -243,11 +300,11 @@ def folder_parser(sup_parser: argparse):
     populate.add_argument('-fid',
                           '--folder_id',
                           required=True,
-                          type=int)
+                          type=valid_int)
     populate.add_argument('-tid',
                           '--task_id',
                           required=True,
-                          type=int)
+                          type=valid_int)
 
     unpopulate = folder_subparser.add_parser(
         'unpopulate',
@@ -255,24 +312,24 @@ def folder_parser(sup_parser: argparse):
     unpopulate.add_argument('-fid',
                             '--folder_id',
                             required=True,
-                            type=int)
+                            type=valid_int)
     unpopulate.add_argument('-tid',
                             '--task_id',
                             required=True,
-                            type=int)
+                            type=valid_int)
 
     edit = folder_subparser.add_parser(
         'edit',
         help='Edit folder by id')
     edit.add_argument('folder_id',
-                      type=int)
+                      type=valid_int)
     edit.add_argument('name',
                       help='Folder name')
 
     delete = folder_subparser.add_parser('delete',
                                          help='Delete folder by id')
     delete.add_argument('folder_id',
-                        type=int)
+                        type=valid_int)
 
 
 def plan_show_parser(sup_parser: argparse):
@@ -280,12 +337,14 @@ def plan_show_parser(sup_parser: argparse):
                                           title='Show plan and its info',
                                           metavar='',
                                           description='Commands to show plans')
+    plan_show.required = True
+
     plan_id = plan_show.add_parser('id',
                                    help='Show plan by id')
 
     plan_id.add_argument('plan_id',
                          help='Plan id',
-                         type=int)
+                         type=valid_int)
     plan_id.add_argument('--tasks',
                          action='store_true',
                          help='Show plan. Task and generated tasks')
@@ -305,23 +364,29 @@ def plan_parser(sup_parser: argparse):
                                                 metavar='',
                                                 description='Commands to work with plans')
 
+    plan_subparser.required = True
+
     create = plan_subparser.add_parser('create',
                                        help='Create plan for existing task')
     create.add_argument('task_id',
                         help='task id',
-                        type=int)
+                        type=valid_int)
     create.add_argument('period_amount',
                         help='Period amount',
-                        type=int)
+                        type=valid_int)
     create.add_argument('period',
                         help='Period type',
                         choices=[x.name.lower() for x in Period])
 
+    create.add_argument('-s', '--start_date',
+                        type=valid_future_date,
+                        help='Plan start date.',
+                        required=True)
     create.add_argument('-r', '--plan_amount',
-                        type=int,
+                        type=valid_int,
                         help='How much times plan should be executed')
     create.add_argument('-e', '--end_date',
-                        type=valid_date,
+                        type=valid_future_date,
                         help='Plan end date.')
 
     show = plan_subparser.add_parser('show', help='Show plans info')
@@ -331,23 +396,23 @@ def plan_parser(sup_parser: argparse):
                                      help='Edit plan')
     edit.add_argument('plan_id',
                       help='plan_id',
-                      type=int)
+                      type=valid_int)
     edit.add_argument('-p', '--period_amount',
                       help='Period amount',
-                      type=int)
+                      type=valid_int)
     edit.add_argument('-t', '--period',
                       help='Period type',
                       choices=[x.name.lower() for x in Period])
     edit.add_argument('-r', '--plan_amount',
-                      type=int,
+                      type=valid_int,
                       help='How much plan should be executed')
     edit.add_argument('-e', '--end_date',
-                      type=valid_date,
+                      type=valid_future_date,
                       help='Plan end date.')
 
     delete = plan_subparser.add_parser('delete',
                                        help='Delete plan by id')
-    delete.add_argument('plan_id', type=int)
+    delete.add_argument('plan_id', type=valid_int)
 
 
 def user_parser(sup_parser: argparse):
@@ -356,6 +421,7 @@ def user_parser(sup_parser: argparse):
     user_subparser = user_parser.add_subparsers(dest='action',
                                                 metavar='',
                                                 description='Commands to work with user')
+    user_subparser.required = True
 
     create = user_subparser.add_parser('create', help='Register new user')
     create.add_argument('username')
@@ -372,12 +438,14 @@ def reminder_show_parser(sup_parser: argparse):
                                               metavar='',
                                               description='Commands to show reminders')
 
+    reminder_show.required = True
+
     reminder_id = reminder_show.add_parser('id',
                                            help='Show reminder by id')
 
     reminder_id.add_argument('reminder_id',
                              help='Reminder id',
-                             type=int)
+                             type=valid_int)
 
     reminder_id.add_argument('--task',
                              action='store_true',
@@ -398,14 +466,16 @@ def reminder_parser(sup_parser: argparse):
                                                         metavar='',
                                                         description='Commands to work with reminders')
 
+    reminder_subparser.required = True
+
     create = reminder_subparser.add_parser('create',
                                            help='Create reminder for existing task')
     create.add_argument('task_id',
                         help='Task id',
-                        type=int)
+                        type=valid_int)
     create.add_argument('date',
                         help='Reminder date',
-                        type=valid_date)
+                        type=valid_future_date)
 
     show = reminder_subparser.add_parser('show', help='Show reminders info')
     reminder_show_parser(show)
@@ -414,14 +484,14 @@ def reminder_parser(sup_parser: argparse):
                                          help='Edit reminder')
     edit.add_argument('reminder_id',
                       help='Reminder id',
-                      type=int)
+                      type=valid_int)
     edit.add_argument('date',
                       help='Reminder date',
-                      type=valid_date)
+                      type=valid_future_date)
 
     delete = reminder_subparser.add_parser('delete',
                                            help='Delete reminder by id')
-    delete.add_argument('reminder_id', type=int)
+    delete.add_argument('reminder_id', type=valid_int)
 
 
 def get_args():
@@ -432,6 +502,8 @@ def get_args():
         dest='entity',
         title='Entities',
         description='Select entity you want to work with', metavar='')
+
+    entity_parser.required = True
 
     user_parser(entity_parser)
     task_parser(entity_parser)
