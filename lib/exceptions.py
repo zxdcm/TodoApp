ffea8b12b356e53aed410f59b3314bd2c06ee6eb
Egor Kirilin <z@xd.cm>
2018-08-23 13:34:13 +0300

    Doc strings for exceptions added

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -1,15 +1,19 @@
 
 class LibError(Exception):
+    """Base lib error class"""
     pass
 
 
 class ObjectNotFound(LibError):
+    """Raises when trying to retrieve object that doesnt exist"""
     pass
 
 
 class LibWarning(Warning):
+    """Base lib warning class"""
     pass
 
 
 class RedundancyAction(LibWarning):
+    """Raises when trying to perform action that already has been done"""
     pass
