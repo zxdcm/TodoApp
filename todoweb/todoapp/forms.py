ee1664c34e83335b122c85f78461d60f2e251330
Egor Kirilin <z@xd.cm>
2018-09-01 20:29:29 +0300

    TaskForm updated. Cleaned_data['folders'] now returns ids in int type FolderForm added.

--- todoweb/todoapp/forms.py
+++ todoweb/todoapp/forms.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from django import forms
-from django.conf import settings
 from django.contrib.auth.models import User
 from todolib.models import TaskStatus, TaskPriority
 from todoapp import get_service
@@ -15,25 +14,31 @@ class TaskForm(forms.Form):
         self.fields['folders'].choices = folders_tuples
 
 
-
     name = forms.CharField(max_length=200, widget=forms.TextInput(
         attrs={'placeholder': 'Task name'}))
     description = forms.CharField(
-        required=False,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': 'Task description'}))
+        required=False)
     priority = forms.ChoiceField(
         choices=[(priority.value, priority.value) for priority in TaskPriority]
     )
     status = forms.ChoiceField(
-        choices=[(status.value, status.value) for status in TaskStatus]
-    )
+        choices=[(status.value, status.value) for status in TaskStatus][:-2])
     event = forms.BooleanField(initial=False, required=False)
     start_date = forms.DateTimeField(required=False, initial=datetime.now())
     end_date = forms.DateTimeField(required=False,
                                    initial=datetime.now() + timedelta(days=1))
 
     assigned = forms.ModelChoiceField(User.objects.all(), required=False)
-    folders = forms.MultipleChoiceField()
+    folders = forms.MultipleChoiceField(required=False)
 
+    def clean_folders(self):
+        data = [*map(int, self.cleaned_data['folders'])]
+        if 0 in data:
+            data.remove(0)
+        return data
+
+
+class FolderForm(forms.Form):
+    name = forms.CharField(max_length=20,
+                           widget=forms.TextInput(
+                            attrs={'placeholder': 'folder name'}))
