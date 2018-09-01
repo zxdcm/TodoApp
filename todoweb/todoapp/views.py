a232876c4ecc8d89e30915c7dfce89715033871d
Egor Kirilin <z@xd.cm>
2018-09-01 20:32:49 +0300

    CRUD for Folders added. Tasks edit method now uses sets to determine from which folders to remove/add task

--- todoweb/todoapp/views.py
+++ todoweb/todoapp/views.py
@@ -4,16 +4,16 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+import logging
 
-
-from todolib.services import  AppService
-from todolib.models import (set_up_connection,
-                            TaskPriority,
+from todolib.models import (TaskPriority,
                             TaskStatus)
 from todolib.exceptions import ObjectNotFound
 from todoapp import get_service
-from .forms import TaskForm
+from .forms import TaskForm, FolderForm
 
+
+logger = logging.getLogger(__name__)
 
 def signup(request):
     if request.method == 'POST':
@@ -31,16 +31,74 @@ def signup(request):
 def index(request):
     return render(request, 'base.html')
 
+
+
+@login_required
+def add_folder(request):
+
+    if request.method == 'POST':
+        form = FolderForm(request.POST)
+        if form.is_valid():
+            service = get_service()
+            name = form.cleaned_data['name']
+            user = request.user.username
+            folder = service.get_folder_by_name(user=user, name=name)
+
+            if folder:
+                form.add_error('name', f'You already have folder {name}')
+                return render(request, 'tasks/add.html', {'form': form})
+
+            folder = service.create_folder(user=user, name=name)
+            return redirect('todoapp:folder_tasks', folder.id)
+
+    else:
+        form = FolderForm()
+
+    return render(request, 'folders/add.html', {'form': form})
+
+@login_required
+def edit_folder(request, id):
+
+    if request.method == 'POST':
+        form = FolderForm(request.POST)
+        if form.is_valid():
+            service = get_service()
+            user = request.user.username
+            name = form.cleaned_data['name']
+            folder = service.get_folder_by_name(user=user, name=name)
+            logger.info('id type',type(id))
+            if folder and folder.id is not id:
+                form.add_error('name', f'You already have folder {name}')
+                return render(request, 'tasks/edit.html', {'form':form})
+
+            service.update_folder(user=user, folder_id=id, name=name)
+            return redirect('todoapp:folder_tasks', id)
+
+    else:
+        form = FolderForm()
+
+    return render(request, 'folders/edit.html', {'form': form})
+
+@login_required
+def delete_folder(request, id):
+    if request.method == 'POST':
+        service = get_service()
+        user = request.user.username
+        service.delete_folder(user=user,
+                              folder_id=id)
+    return redirect('todoapp:tasks')
+
+
 @login_required
 def create_task(request):
 
-    service = get_service()
     user = request.user.username
 
     if request.method == 'POST':
         form = TaskForm(user, request.POST)
         if form.is_valid():
             try:
+                service = get_service()
                 assigned = form.cleaned_data['assigned']
                 if assigned:
                     assigned = assigned.username
@@ -59,9 +117,8 @@ def create_task(request):
                 return render(request, 'tasks/add.html', {'form': form})
 
             folders_ids = form.cleaned_data['folders']
-            if '0' in folders_ids:
-                folders_ids.remove('0')
-            for folder_id in form.cleaned_data['folders']:
+
+            for folder_id in folders_ids:
                 service.populate_folder(user=user,
                                         folder_id=folder_id,
                                         task_id=task.id)
@@ -73,6 +130,7 @@ def create_task(request):
 
 @login_required
 def edit_task(request, id):
+
     service = get_service()
     user = request.user.username
 
@@ -107,29 +165,36 @@ def edit_task(request, id):
             else:
                 task.assigned = None
 
-            folders = service.get_task_folders(task.id)
-            for folder in folders:
-                folder.tasks.remove(task)
+            current_folders = service.get_task_folders(task_id=task.id,
+                                                       user=user)
 
-            folders_ids = form.cleaned_data['folders']
-            if '0' in folders_ids:
-                folders_ids.remove('0')
-            for folder_id in folders_ids:
-                folder = service.get_folder(user=user,
-                                            folder_id=folder_id)
-                folder.tasks.append(task)
+            old_folders = set(map(lambda x: x.id, current_folders))
+            new_folders = set(form.cleaned_data['folders'])
 
-            service.save_updates()
-            return redirect('todoapp:tasks')
+            add = new_folders.difference(old_folders)
+            remove = old_folders.difference(new_folders)
+
+            for folder_id in add:
+                service.populate_folder(user=user,
+                                        folder_id=folder_id,
+                                        task_id=task.id)
+            for folder_id in remove:
+                service.unpopulate_folder(user=user,
+                                          folder_id=folder_id,
+                                          task_id=task.id)
+
+            return redirect('todoapp:show_task', task.id)
 
     else:
         assigned = None
         if task.assigned:
             assigned = User.objects.filter(username=task.assigned).get()
 
-        initial_folders = [folder.id for folder in service.get_task_folders(task.id)
-                           if task.id in map(lambda x: x.id, folder.tasks)]
+        initial_folders = [folder.id for folder
+                           in service.get_task_folders(task.id, user=user)]
 
+        if not initial_folders:
+            initial_folders = 0
         form = TaskForm(
             user,
             initial={
@@ -149,8 +214,9 @@ def edit_task(request, id):
 @login_required()
 def show_task(request, id):
     try:
-       task =get_service().get_task(user=request.user.username,
-                                    task_id=id)
+        user = request.user.username
+        task = get_service().get_task(user=user,
+                                      task_id=id)
     except ObjectNotFound:
         return redirect('todoapp:tasks')
     return render(request, 'tasks/show.html', {'task': task})
@@ -167,8 +233,9 @@ def delete_task(request):
 @login_required
 def own_tasks(request):
     service = get_service()
-    tasks = service.get_own_tasks(request.user.username)
-    folders = service.get_all_folders(user=request.user.username)
+    user = request.user.username
+    tasks = service.get_own_tasks(user)
+    folders = service.get_all_folders(user)
     args = {'tasks':tasks, 'header': 'My tasks',
             'folders': folders}
     return render(request, 'tasks/list.html', args)
@@ -176,8 +243,9 @@ def own_tasks(request):
 @login_required
 def available_tasks(request):
     service = get_service()
-    tasks = service.get_available_tasks(request.user.username)
-    folders = service.get_all_folders(user=request.user.username)
+    user = request.user.username
+    tasks = service.get_available_tasks(user)
+    folders = service.get_all_folders(user)
     args = {'tasks':tasks, 'header': 'Available tasks',
             'folders': folders}
     return render(request, 'tasks/list.html', args)
@@ -186,8 +254,9 @@ def available_tasks(request):
 @login_required
 def assigned_tasks(request):
     service = get_service()
-    tasks = service.get_user_assigned_tasks(request.user.username)
-    folders = service.get_all_folders(user=request.user.username)
+    user = request.user.username
+    tasks = service.get_user_assigned_tasks(user)
+    folders = service.get_all_folders(user)
     args = {'tasks':tasks, 'header': 'Assigned tasks',
             'folders': folders}
     return render(request, 'tasks/list.html', args)
@@ -195,9 +264,10 @@ def assigned_tasks(request):
 @login_required
 def archived_tasks(request):
     service = get_service()
-    tasks = service.get_filtered_tasks(request.user.username,
+    user = request.user.username
+    tasks = service.get_filtered_tasks(user=user,
                                        status=TaskStatus.ARCHIVED)
-    folders = service.get_all_folders(user=request.user.username)
+    folders = service.get_all_folders(user)
     args = {'tasks':tasks, 'header': 'Archived tasks',
             'folders': folders}
     return render(request, 'tasks/list.html', args)
@@ -206,9 +276,10 @@ def archived_tasks(request):
 @login_required
 def done_tasks(request):
     service = get_service()
-    tasks = service.get_filtered_tasks(request.user.username,
+    user = request.user.username
+    tasks = service.get_filtered_tasks(user=user,
                                        status=TaskStatus.DONE)
-    folders = service.get_all_folders(user=request.user.username)
+    folders = service.get_all_folders(user)
     args = {'tasks':tasks, 'header': 'Done tasks',
             'folders': folders}
     return render(request, 'tasks/list.html', args)
@@ -216,20 +287,17 @@ def done_tasks(request):
 @login_required
 def folder_tasks(request, id):
     service = get_service()
+    user = request.user.username
     try:
-        folder =service.get_folder(user=request.user.username,
+        folder =service.get_folder(user=user,
                                    folder_id=id)
     except:
         return redirect('todoapp:tasks')
-    folders = service.get_all_folders(user=request.user.username)
+    folders = service.get_all_folders(user=user)
     args = {'tasks':folder.tasks, 'header': f'{folder.name} tasks',
             'folders':folders}
     return render(request, 'tasks/list.html', args)
 
-
-@login_required
-def add_folder(request):
-    return render(request, 'tasks/list.html')
 
 def dashboard_test(request):
     return render(request, 'Dashboard Template for Bootstrap.html')
