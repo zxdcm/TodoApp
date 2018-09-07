59aa5262c50b2aac006855619967a941cd39e642
Egor Kirilin <z@xd.cm>
2018-09-07 23:48:06 +0300

    FilterForm added. All forms refactoried coz of discover of some forms features

--- todoweb/todoapp/forms.py
+++ todoweb/todoapp/forms.py
@@ -21,34 +21,40 @@ class TaskForm(forms.Form):
     name = forms.CharField(max_length=200, widget=forms.TextInput(
         attrs={'placeholder': 'Task name'}))
     description = forms.CharField(
-        required=False)
+        required=False,
+        empty_value=None)
     priority = forms.ChoiceField(
         choices=[(priority.value, priority.value) for priority in TaskPriority]
     )
     status = forms.ChoiceField(
-        choices=[(status.value, status.value) for status in TaskStatus][:-1])
+        choices=[(status.value, status.value) for status in TaskStatus][:-1],
+        required=False)
     event = forms.BooleanField(initial=False, required=False)
-    start_date = forms.DateTimeField(required=True, initial=datetime.now())
-    end_date = forms.DateTimeField(required=True,
-                                   initial=datetime.now() + timedelta(days=1))
+    start_date = forms.DateTimeField(initial=datetime.now())
+    end_date = forms.DateTimeField(initial=datetime.now() + timedelta(days=1))
 
     assigned = forms.ModelChoiceField(User.objects.all(), required=False)
-    folders = forms.MultipleChoiceField(required=False)
+    folders = forms.TypedMultipleChoiceField(required=False,
+                                             coerce=int,
+                                             empty_value=0)
 
     def clean_folders(self):
-        data = [*map(int, self.cleaned_data['folders'])]
+        data = self.cleaned_data['folders']
         if 0 in data:
             data.remove(0)
         return data
 
+    def clean_assigned(self):
+        if self.cleaned_data['assigned']:
+            return self.cleaned_data['assigned'].username
 
     def clean_start_date(self):
         if self.cleaned_data['start_date']:
-            self.cleaned_data['start_date'].replace(tzinfo=None)
+            return self.cleaned_data['start_date'].replace(tzinfo=None)
 
     def clean_end_date(self):
         if self.cleaned_data['end_date']:
-            self.cleaned_data['end_date'].replace(tzinfo=None)
+            return self.cleaned_data['end_date'].replace(tzinfo=None)
 
 
 
@@ -60,7 +66,7 @@ class FolderForm(forms.Form):
     
 class SubTaskForm(forms.Form):
 
-    task_id = forms.ChoiceField(required=True)
+    task_id = forms.TypedChoiceField(coerce=int)
 
     def __init__(self, user, *args, **kwargs):
         super(SubTaskForm, self).__init__(*args, **kwargs)
@@ -73,10 +79,6 @@ class SubTaskForm(forms.Form):
         self.fields['task_id'] = forms.ChoiceField(choices=choices)
 
 
-    def clean_task_id(self):
-        return int(self.cleaned_data['task_id'])
-    
-    
 class MemberForm(forms.Form):
 
     member = forms.ModelChoiceField(User.objects.all(), required=True)
@@ -102,33 +104,26 @@ class PlanForm(forms.Form):
             self.fields['start_date'].required = False
 
 
-    task_id = forms.ChoiceField()
+    task_id = forms.TypedChoiceField(coerce=int)
 
     period_amount = forms.IntegerField(
-        validators=[MinValueValidator(1)],
-        required=True)
+        validators=[MinValueValidator(1)])
     period = forms.ChoiceField(
-        choices=[(period.value, period.value) for period in Period],
-        required=True)
+        choices=[(period.value, period.value) for period in Period])
 
     repetitions_amount = forms.IntegerField(
-        validators=[MinValueValidator(1)],
+        validators=[MinValueValidator(0)],
         required=False)
 
-    start_date = forms.DateTimeField(required=True, initial=datetime.now())
+    start_date = forms.DateTimeField(initial=datetime.now())
     end_date = forms.DateTimeField(required=False)
 
-
-    def clean_task_id(self):
-        if self.cleaned_data['task_id']:
-            return int(self.cleaned_data['task_id'])
 
     def clean_end_date(self):
         if self.cleaned_data['end_date']:
             date = self.cleaned_data['end_date'].replace(tzinfo=None)
             if date < datetime.now():
                 msg = 'End date should be in future.'
-                # self._errors['end_date'] = [msg]
                 self.add_error('end_date', msg)
             return date
 
@@ -136,23 +131,25 @@ class PlanForm(forms.Form):
         if self.cleaned_data['start_date']:
             date = self.cleaned_data['start_date'].replace(tzinfo=None)
             if date < datetime.now():
-                msg = 'Start date should be in future.'
-                self.add_error('start_date',msg)
+                # dont show errors if use form for edit existing plan.
+                readonly = self.fields['start_date'].widget.attrs.get('readonly', None)
+                if readonly is not True:
+                    msg = 'Start date should be in future.'
+                    self.add_error('start_date', msg)
             return date
 
     def clean(self):
         start_date = self.cleaned_data['start_date']
         end_date = self.cleaned_data.get('end_date', None)
-        if start_date and end_date:
-            if end_date < start_date:
-                msg = 'End date should be greater than start date.'
-                self._errors['end_date'] = [msg]
+        if (start_date and end_date) and (end_date < start_date):
+            msg = 'End date should be greater than start date.'
+            self.add_error('start_date', msg)
 
 
 class ReminderForm(forms.Form):
 
-    task_id = forms.ChoiceField(required=True)
-    date = forms.DateTimeField(required=True)
+    task_id = forms.TypedChoiceField(coerce=int)
+    date = forms.DateTimeField()
 
     def __init__(self, user, *args, **kwargs):
         super(ReminderForm, self).__init__(*args, **kwargs)
@@ -161,10 +158,83 @@ class ReminderForm(forms.Form):
         choices = ((task.id, f'ID: {task.id} Name: {task.name}') for task in tasks)
         self.fields['task_id'] = forms.ChoiceField(choices=choices)
 
-
-    def clean_task_id(self):
-        return int(self.cleaned_data['task_id'])
-
     def clean_date(self):
-        return self.cleaned_data['date'].replace(tzinfo=None)
+        if self.cleaned_data['date']:
+            return self.cleaned_data['date'].replace(tzinfo=None)
 
+
+class TaskFilterForm(forms.Form):
+
+    def __init__(self, user, *args, **kwargs):
+        super(TaskFilterForm, self).__init__(*args, **kwargs)
+
+        service = get_service()
+
+        priority_choices = [(priority.value, priority.value) for priority in TaskPriority]
+        priority_choices.insert(0, (None, '---------'))
+
+        status_choices = [(status.value, status.value) for status in TaskStatus]
+        status_choices.insert(0, (None, '---------'))
+
+        parent_tasks = service.get_available_tasks(user=user)
+
+        parent_tasks_choices = [(task.id, f'ID: {task.id} Name: {task.name}') for task in parent_tasks]
+
+        parent_tasks_choices.insert(0, (None, '---------'))
+
+        self.fields['status'].choices = status_choices
+        self.fields['priority'].choices = priority_choices
+        self.fields['parent_task_id'].choices = parent_tasks_choices
+
+
+    name = forms.CharField(max_length=200,
+                           widget=forms.TextInput(),
+                           required=False,
+                           empty_value=None)
+
+    owner = forms.ModelChoiceField(User.objects.all(),
+                                   required=False)
+
+    description = forms.CharField(required=False, empty_value=None)
+
+    priority = forms.TypedChoiceField(required=False,
+                                      empty_value=None)
+
+    status = forms.TypedChoiceField(required=False,
+                                    empty_value=None)
+
+    parent_task_id = forms.TypedChoiceField(label='Parent task',
+                                            required=False,
+                                            coerce=int,
+                                            empty_value=None)
+
+    event = forms.NullBooleanField(required=False)
+
+    start_date = forms.DateTimeField(required=False,
+                                     help_text='From start date')
+    end_date = forms.DateTimeField(required=False,
+                                   help_text='Till end date')
+
+    assigned = forms.ModelChoiceField(User.objects.all(),
+                                      required=False)
+
+    def clean_owner(self):
+        if self.cleaned_data['owner']:
+            return self.cleaned_data['owner'].username
+
+    def clean_start_date(self):
+        if self.cleaned_data['start_date']:
+            return self.cleaned_data['start_date'].replace(tzinfo=None)
+
+    def clean_end_date(self):
+        if self.cleaned_data['end_date']:
+            return self.cleaned_data['end_date'].replace(tzinfo=None)
+
+    def clean_assigned(self):
+        if self.cleaned_data['assigned']:
+            return self.cleaned_data['assigned'].username
+
+
+    def clean(self):
+        if not any(self.cleaned_data.values()):
+            self.add_error(0, 'You have forgotten to specify params')
